dbc8e938f3b1a13e96c00c5164b6cd3591ddd8e2
Mariangel Acosta <macosta@prevalentware.com>
2024-06-19 20:15:45 -0500

    se confugura shema

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,25 +1,25 @@
 // app/api/auth/[auth0]/route.js
-import { handleAuth } from '@auth0/nextjs-auth0';
-const GET = handleAuth();
-export default GET;
+// import { handleAuth } from '@auth0/nextjs-auth0';
+// const GET = handleAuth();
+// export default GET;
 
-// import { handleAuth, handleCallback, Session } from '@auth0/nextjs-auth0';
-// import { NextApiRequest, NextApiResponse  } from 'next';
+import { handleAuth, handleCallback, Session } from '@auth0/nextjs-auth0';
+import { NextApiRequest, NextApiResponse  } from 'next';
 
-// const afterCallback = async (req: NextApiRequest, res: NextApiResponse , session: Session) => {
-//     const {user} = session;
-//     if(user){
-//         // await 
-//     }
-//     return session; 
-// }
+const afterCallback = async (req: NextApiRequest, res: NextApiResponse , session: Session) => {
+    const {user} = session;
+    if(user){
+        // await 
+    }
+    return session; 
+}
 
-// export default handleAuth({
-//     async callback(req: any, res: any) {
-//         try {
-//            await handleCallback(req, res, {afterCallback}); 
-//         } catch (error: any) {
-//             res.status(error.status || 500).end(error.message)
-//         }
-//     }
-// })
+export default handleAuth({
+    async callback(req: any, res: any) {
+        try {
+           await handleCallback(req, res, {afterCallback}); 
+        } catch (error: any) {
+            res.status(error.status || 500).end(error.message)
+        }
+    }
+})
